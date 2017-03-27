ff9f7b662733a96d17815d7341ab7b9d3c4b5192
Bekker,David D.S.H. de <david.de.bekker@live.nl>
2017-03-28 01:01:34 +0200

    Major authentification update
    Cleaner code. No PropertyHandler class needed. Better method
    distribution. No need for passing objects between activities.

--- app/src/main/java/nextweek/fontys/next/app/Activities/LogActivity.java
+++ app/src/main/java/nextweek/fontys/next/app/Activities/LogActivity.java
@@ -1,10 +1,15 @@
 package nextweek.fontys.next.app.Activities;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -12,12 +17,45 @@ import nextweek.fontys.next.R;
 
 public class LogActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
 
+        mAuth = FirebaseAuth.getInstance();
+        signIn("david.de.bekker@live.nl", "asdfasdf");
+    }
+
+    /**
+     * Tries to sign in a user with an email and a password
+     * @param email of the user
+     * @param password of the user
+     */
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LogActivity.this, "Fail!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    /**
+     * Switches to the log activity.
+     */
+    private void openLogActivity() {
+        Intent intent = new Intent(this, LogActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/nextweek/fontys/next/app/Activities/SplashActivity.java
+++ app/src/main/java/nextweek/fontys/next/app/Activities/SplashActivity.java
@@ -12,32 +12,20 @@ import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.logging.Logger;
-
 import io.codetail.animation.ViewAnimationUtils;
 import nextweek.fontys.next.R;
-import nextweek.fontys.next.app.Models.PropertyHandler;
 
 public class SplashActivity extends AppCompatActivity {
 
     private final static int ANIMATION_TIME_CONST = 1000;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseAuth mAuth;
-    private boolean authenticated = false;
-    private boolean done = false;
-    private Object lock;
+
+    private boolean signed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +39,12 @@ public class SplashActivity extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     // User is signed in
-                    Log.d("SplashActivity", "onAuthStateChanged:signed_in:" + user.getUid());
+                    Log.d("SplashActivity", "onAuthStateChanged:signed_in:" + user.getEmail());
+                    signed = true;
                 } else {
                     // User is signed out
                     Log.d("SplashActivity", "onAuthStateChanged:signed_out");
+                    signed = false;
                 }
             }
         };
@@ -78,7 +68,11 @@ public class SplashActivity extends AppCompatActivity {
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
-                                checkLogProperties();
+                                if (signed) {
+                                    openScanActivity();
+                                } else {
+                                    openLogActivity();
+                                }
                             }
                         }, ANIMATION_TIME_CONST);
                     }
@@ -99,28 +93,6 @@ public class SplashActivity extends AppCompatActivity {
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
-    }
-
-    private boolean signIn(String email, String password) {
-        Log.d("signIn", "createUserWithEmail:onComplete LOGGINGISENFSHFHSIUEFHUISEHFUISEHFUISEHFUIH");
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("signIn", "createUserWithEmail:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (task.isSuccessful()) {
-                            authenticated = true;
-                        }
-                        unlockFXThread();
-                    }
-                });
-
-        lockFXThread();
-        return authenticated;
     }
 
     /**
@@ -157,22 +129,6 @@ public class SplashActivity extends AppCompatActivity {
         view.startAnimation(animationFadeIn);
     }
 
-    private void checkLogProperties() {
-        try {
-            ExecutorService pool = Executors.newFixedThreadPool(1);
-            Future<String[]> futureData = pool.submit(new PropertyHandler(this));
-            String[] data = futureData.get();
-
-            if (signIn("david.de.bekker@live.nl", "asdfasdf")) {
-                openScanActivity();
-            } else {
-                openLogActivity();
-            }
-        } catch (InterruptedException | ExecutionException ex) {
-            Log.e("SplashActivity", "Exception", ex);
-        }
-    }
-
     /**
      * Switches to the scan activity.
      */
@@ -181,36 +137,11 @@ public class SplashActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /**
+     * Switches to the log activity.
+     */
     private void openLogActivity() {
         Intent intent = new Intent(this, LogActivity.class);
         startActivity(intent);
-    }
-
-    /**
-     * Tells a random object to wait while in a loop.
-     * The loop stops, and won't cause any unnecessary cpu use.
-     */
-    private void lockFXThread() {
-        lock = new Object();
-        synchronized (lock) {
-            while (!done) {
-                try {
-                    lock.wait();
-                } catch (InterruptedException ex) {
-                    Log.e("SplashActivity", "Exception", ex);
-                }
-            }
-        }
-        done = false;
-    }
-
-    /**
-     * Wakes the lock. The while loop in the method 'lockFXThread' will proceed and break free.
-     */
-    private void unlockFXThread() {
-        synchronized (lock) {
-            done = true;
-            lock.notifyAll();
-        }
     }
 }
